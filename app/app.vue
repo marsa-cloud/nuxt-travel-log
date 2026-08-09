90b9bca74f69c5b96caff9f0c6b786ac10fa5778
Mohammad Gomaa <midomaxgomaa@gmail.com>
2026-08-09 12:26:16 +0300

    feat: landing apge

--- app/app.vue
+++ app/app.vue
@@ -1,13 +1,5 @@
 <template>
-  <div>
-    <!-- <NuxtRouteAnnouncer />
-    <NuxtWelcome /> -->
-    <h1 class="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    <button class="btn btn-secondary">
-      Click me
-    </button>
-    <Icon name="tabler:map" />
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
---
+++ app/pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="hero container mx-auto mt-4 bg-base-300">
+    <div class="hero-content min-h-96 text-center">
+      <div class="max-w-md">
+        <h1 class="text-5xl font-bold">
+          Travel Log
+        </h1>
+        <p class="py-6">
+          Keep track of your travels and adventures with this simple travle log app. Add locatiosn, photos and notes to create a digital journal of your journeys.
+        </p>
+        <button class="btn btn-accent">
+          Sign In with GitHub
+          <Icon name="tabler:brand-github" size="24" />
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
